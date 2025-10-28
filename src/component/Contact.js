c93e61dd796c7b2e307629e7e55afe583d9d6844
QudratUllah <qudratullahmasoom6688@gmail.com>
2025-10-28 03:26:55 -0700

    i write parent in contact

---
+++ src/component/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact(props) {
+  return (
+    <>
+    <div className="parent">
+        <h1>{props.qudrat}</h1>
+    </div>
+    </>
+  )
+}
