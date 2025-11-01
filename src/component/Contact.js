31a0665c44d02b2b4e1ba991beddc8b116d6aba6
QudratUllah <qudratullahmasoom6688@gmail.com>
2025-11-01 03:21:54 -0700

    i write html css in contact.js

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
+
 export default function Contact(props) {
   return (
     <>
     <div className="parent1">
-        <h1 id='h1_contact'>{props.qudrat}</h1>
+        <h1>{props.qudrat}</h1>
     </div>
+    <p>Inter your Text For Edit</p>
+    <textarea name="text" id="text" rows={7}></textarea>
+    <button id='btn'>Change To Uppercase</button>
+    <button id='btn'>Change To Lowercase</button>
+    <button id='btn'>Finish Extra Space</button>
+    <button id='btn'>Copy</button>
+    
     </>
   )
 }
