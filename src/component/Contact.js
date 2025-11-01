441e9631158f9804b58de0b719f55af9624a7c60
QudratUllah <qudratullahmasoom6688@gmail.com>
2025-11-01 04:20:23 -0700

    i add the js to button

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import React,{useState} from 'react'
 
 
 export default function Contact(props) {
+  const [text, changetext] = useState('')
+  const handlevalue = (e)=>{
+    changetext(e.target.value)
+  }
+  const touppercase = ()=>{
+    changetext.touppercase();
+  }
   return (
     <>
     <div className="parent1">
@@ -9,9 +17,9 @@ export default function Contact(props) {
     </div>
     <div className="child">
     <p>Inter your Text For Edit</p>
-    <textarea name="text" id="text" rows={7}></textarea>
+    <textarea name="text" id="text" rows={7} value={text} onChange={handlevalue} ></textarea>
       <div className="box1">
-      <button id='btn'>Change To Uppercase</button>
+      <button id='btn' onClick={touppercase}>Change To Uppercase</button>
       <button id='btn'>Change To Lowercase</button>
       <button id='btn'>Finish Extra Space</button>
       <button id='btn'>Copy</button>
