18fe17b89ef3c6caa9fbf5972a77889727d3aa99
QudratUllah <qudratullahmasoom6688@gmail.com>
2025-11-02 00:35:33 -0700

    i add the extra btn action

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,12 +1,8 @@
-
-
-
-
-import React, {useState} from 'react'
-
+import React, {useState ,useRef} from 'react'
 
 export default function Contact(props) {
   const [text, changetext] = useState('')
+  const textref = useRef();
   const handlevalue = (e)=>{
     changetext(e.target.value)
   }
@@ -14,6 +10,9 @@ export default function Contact(props) {
     changetext(text.toUpperCase())}
     const tolowercase = ()=>{
     changetext(text.toLowerCase())}
+  const extraspace = ()=>{
+    navigator.clipboard.writeText(textref.current.value);
+  }
   return (
     <>
     <div className="parent1">
@@ -21,11 +20,11 @@ export default function Contact(props) {
     </div>
     <div className="child">
     <p>Inter your Text For Edit</p>
-    <textarea name="text" id="text" rows={7} value={text} onChange={handlevalue} ></textarea>
+    <textarea name="text" id="text" rows={7} ref={textref} value={text} onChange={handlevalue} ></textarea>
       <div className="box1">
       <button id='btn' onClick={touppercase}>Change To Uppercase</button>
       <button id='btn' onClick={tolowercase}>Change To Lowercase</button>
-      <button id='btn'>Finish Extra Space</button>
+      <button id='btn' onClick={extraspace}>Finish Extra Space</button>
       <button id='btn'>Copy</button>
       </div>
     </div>
