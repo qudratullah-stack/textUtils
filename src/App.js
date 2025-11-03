d92dcdc5be8ef1a7f520dcc24fc884aadac0af9b
QudratUllah <qudratullahmasoom6688@gmail.com>
2025-11-02 21:45:17 -0800

    i add the alert.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Contact from './component/Contact';
+import alert from './component/alert';
 
 function App() {
   return (
     <>
     <Contact qudrat = 'Welcome Text Editor app'/>
+    <alert massage = 'this is alert'/>
     </>
   );
 }
---
+++ src/component/alert.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function alert(props) {
+  return (
+    <>
+    <div className='alert'>
+        {props.massage}
+    </div>
+    </>
+  )
+}
