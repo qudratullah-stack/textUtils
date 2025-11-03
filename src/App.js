347480d58d776971c56cc7ade9b34d659c344b12
QudratUllah <qudratullahmasoom6688@gmail.com>
2025-11-02 23:16:44 -0800

    i deleted alert file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
+
+
 import Contact from './component/Contact';
-import alert from './component/alert';
+
+
 
 function App() {
   return (
     <>
     <Contact qudrat = 'Welcome Text Editor app'/>
-    <alert massage = 'this is alert'/>
+    
     </>
   );
 }
